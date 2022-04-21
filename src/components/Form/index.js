7267ec4d5fa9011884b01436ef103eeb6cdefb4b
zeldaababil19 <zeldaababil01@gmail.com>
2022-04-21 13:47:01 +0700

    minor'

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,20 +3,17 @@ const Form = (props) => {
     <section className="playlist-form">
       <form id="form" className="form" onSubmit={props.handlePlaylistSubmit}>
         <h2 className="heading">Create Playlist</h2>
-        <ul>
-          <li>
-            <label htmlFor="title" className="form__label">
-              Title
-            </label>
-            <input id="title" type="text" name="title" required minLength="10" placeholder="Add a title" className="form__input" onChange={props.handlePlaylistChange} value={props.playlist.title} />
-          </li>
-          <li>
-            <label htmlFor="description" className="form__label">
-              Description
-            </label>
-            <textarea id="description" name="description" placeholder="Add an optional description" className="form__textarea" onChange={props.handlePlaylistChange} value={props.playlist.description}></textarea>
-          </li>
-        </ul>
+        {/* <ul> */}
+        {/* <li> */}
+        <div className="form-group">
+          <label htmlFor="playlist-name">Playlist Name</label>
+        </div>
+        <input id="title" type="text" name="title" required minLength="10" placeholder="Add a title" className="form__input" onChange={props.handlePlaylistChange} value={props.playlist.title} />
+        {/* </li> */}
+        {/* <li> */}
+        <textarea id="description" name="description" placeholder="Add an optional description" className="form__textarea" onChange={props.handlePlaylistChange} value={props.playlist.description}></textarea>
+        {/* </li> */}
+        {/* </ul> */}
         <button type="submit" className="btn btn--save" disabled={props.isSelectedEmpty}>
           Create playlist
         </button>
